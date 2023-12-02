4c5614b8dfcc22a42372da1bf0004b85e0147a91
Yuval Dolev <yuvaldolev2@gmail.com>
2023-12-02 18:35:02 +0200

    feat: complete the shared state section of the tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::{Mutex, Arc}};
 
+use bytes::Bytes;
 use mini_redis::{Connection, Command, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    
+
+    let db: Arc<Mutex<HashMap<String, Bytes>>> = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
+async fn process(socket: TcpStream, db: Arc<Mutex<HashMap<String, Bytes>>>) {
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Command::Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
             }
             Command::Set(cmd) => {
-                db.insert(cmd.key().to_owned(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_owned(), cmd.value().clone());
                 Frame::Simple(String::from("OK"))
             }
             cmd => panic!("unimplemented: {:?}", cmd),
